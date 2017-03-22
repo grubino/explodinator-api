902fdb956484e683e18dbcff55dee2ea18b72072
Greg Rubino <bibil.thaysose@gmail.com>
2017-03-21 20:40:39 -0400

    lifting on to flask

---
+++ app.py
@@ -0,0 +1,44 @@
+import os
+
+import flask
+from flask import request, redirect, url_for
+from flask.helpers import send_from_directory
+from werkzeug.utils import secure_filename
+
+from imaging.explodinator import Explodinator
+from PIL import Image
+
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), '.tmp')
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+app = flask.Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def _frame_gen(frames=[]):
+    for f in frames:
+        mode = 'RGB'
+        overlay = Image.frombytes(mode, f.size, os.urandom(f.size[0]*f.size[1]*len(mode)))
+        mask = Image.frombytes('1', f.size, os.urandom(f.size[0]*f.size[1]*len(mode)))
+        yield Image.composite(f, overlay, mask)
+
+
+@app.route("/explodinate", methods=['GET', 'POST'])
+def explodinate():
+    if request.method == 'POST' and 'frame0.jpg' in request.files:
+        f = request.files['frame0.jpg']
+        fname = secure_filename(f.name)
+        fpath = os.path.join(app.config['UPLOAD_FOLDER'], fname)
+        f.save(fpath)
+        explodinated_fpath = Explodinator(fpath, _frame_gen).explodinate()
+        return redirect(url_for("explodinated", filename=explodinated_fpath))
+
+
+@app.route("/explodinated/<filename>")
+def explodinated(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == "__main__":
+    app.run(host=os.getenv("EXPLODINATOR_HOST", "localhost"),
+            port=os.getenv("EXPLODINATOR_PORT", "8823"))
--- imaging/explodinator.py
+++ imaging/explodinator.py
@@ -2,25 +2,33 @@ import os
 import sys
 
 import imageio
-from PIL import Image, ImageSequence
+import time
+from PIL import Image
 from functools import reduce
+
+
+def identity_frame_gen(*frames):
+    for f in frames:
+        yield f
+
 
 class Explodinator:
 
-    def __init__(self, fp):
+    def __init__(self, fp, gen=lambda x: x):
         self._original = Image.open(fp)
         self._frames = reduce(lambda x, y: x + [self._original.copy()], range(48), [])
-        self._outfp = '.'.join([fp.split('.')[0], 'gif'])
+        self._outfp = '.'.join([os.path.splitext(fp)[0], 'gif'])
+        self._gen_frames = gen
 
     def explodinate(self):
         filenames = []
         with imageio.get_writer(self._outfp, mode='I') as writer:
-            for i, f in enumerate(self._frames):
-                _fp = "{2}{1}__{0}".format(os.path.basename(self._outfp), i, os.path.dirname(self._outfp))
+            for i, f in enumerate(self._gen_frames(self._frames)):
+                _fp = "{2}/{1}__{0}".format(os.path.basename(self._outfp), i, os.path.dirname(self._outfp))
                 filenames.append(_fp)
                 f.save(_fp)
                 writer.append_data(imageio.imread(_fp))
-
+        return self._outfp
 
 
 
@@ -29,4 +37,12 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         raise Exception("not enough arguments")
 
-    Explodinator(sys.argv[1]).explodinate()
+    def _randomizer_gen(frames=[]):
+
+        for f in frames:
+            mode = 'RGB'
+            overlay = Image.frombytes(mode, f.size, os.urandom(f.size[0]*f.size[1]*len(mode)))
+            mask = Image.frombytes('1', f.size, os.urandom(f.size[0]*f.size[1]*len(mode)))
+            yield Image.composite(f, overlay, mask)
+
+    Explodinator(sys.argv[1], _randomizer_gen).explodinate()
