5df77eab7723d6d3221093f33e38bdd69b7f8e81
Greg Rubino <bibil.thaysose@gmail.com>
2017-04-14 00:59:05 -0400

    explodinations now using pgmagick

--- app.py
+++ app.py
@@ -4,25 +4,50 @@ import uuid
 import flask
 from flask import request, redirect, url_for
 from flask.helpers import send_from_directory
+from pgmagick import Color, ImageList, Image, Blob, Geometry, CompositeOperator
 from werkzeug.utils import secure_filename
 
 from imaging.explodinator import Explodinator
-from PIL import Image
 
-ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
 UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), '.tmp')
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 app = flask.Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+composite_path = os.path.join(os.path.dirname(__file__), 'imaging', 'resources', 'explodinate_composite.gif')
 
-def _frame_gen(frames=[]):
-    for f in frames:
-        mode = 'RGB'
-        overlay = Image.frombytes(mode, f.size, os.urandom(f.size[0]*f.size[1]*len(mode)))
-        mask = Image.frombytes('1', f.size, os.urandom(f.size[0]*f.size[1]*len(mode)))
-        yield Image.composite(f, overlay, mask)
+
+def resize(img, new_size):
+    g = new_size  # distinguishes whether width, height or both given
+    image = img
+    sz = image.size()
+    rw, rh = (sz.width(), sz.height())
+    w, h = g.width(), g.height()
+    if h > rh:
+        g.height(int(rh * w * 1.0 / rw))
+    elif w > rw:
+        g.width(int(rw * h * 1.0 / rh))
+    image.scale(g)
+    return image
+
+def _frame_gen(frames=(), padding=15):
+    overlay_list = ImageList()
+    overlay_list.readImages(composite_path)
+    for _ in range(10):
+        yield frames[0]
+    for i, overlay in enumerate(overlay_list):
+        if i >= len(frames):
+            break
+        _size = overlay.size()
+        resized = resize(frames[i], _size)
+        resized.implode(float(i) / 5.0)
+        resized.composite(overlay, 0, padding, CompositeOperator(1))
+        yield resized
+    for i in range(10):
+        frames[0].implode(float(len(overlay_list) + i) / 5.0)
+        yield frames[0]
 
 
 @app.route("/explodinate", methods=['POST'])
@@ -30,10 +55,10 @@ def explodinate():
     if request.method == 'POST' and 'frame0.jpg' in request.files:
         f = request.files['frame0.jpg']
         fname = secure_filename(f.name)
-        fpath = "-".join((str(uuid.uuid1()), fname))
+        fpath = os.path.join(app.config['UPLOAD_FOLDER'], "-".join((str(uuid.uuid1()), fname)))
         f.save(fpath)
         explodinated_fpath = Explodinator(fpath, _frame_gen).explodinate()
-        return redirect(url_for("explodinated", filename=explodinated_fpath))
+        return redirect(url_for("explodinated", filename=os.path.basename(explodinated_fpath)))
 
 
 @app.route("/explodinated/<filename>", methods=['GET'])
@@ -41,5 +66,8 @@ def explodinated(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
+    this_dir = os.path.dirname(__file__)
     app.run(host=os.getenv("EXPLODINATOR_HOST", "0.0.0.0"),
-            port=os.getenv("EXPLODINATOR_PORT", "8823"))
+            port=int(os.getenv("EXPLODINATOR_PORT", "8823")),
+            debug=True,
+            extra_files=[f for f in os.walk(this_dir) if f.endswith('.py')])
--- imaging/explodinator.py
+++ imaging/explodinator.py
@@ -1,9 +1,7 @@
 import os
 import sys
 
-import imageio
-import time
-from PIL import Image
+from pgmagick import Image, ImageList, FilterTypes, CompositeOperator as co, Color, Geometry
 from functools import reduce
 
 
@@ -14,20 +12,17 @@ def identity_frame_gen(*frames):
 
 class Explodinator:
 
-    def __init__(self, fp, gen=lambda x: x):
-        self._original = Image.open(fp)
-        self._frames = reduce(lambda x, y: x + [self._original.copy()], range(48), [])
-        self._outfp = '.'.join([os.path.splitext(fp)[0], 'gif'])
+    def __init__(self, fp, gen=lambda x: x, outfp=None):
+        self._original = Image(fp)
+        self._frames = reduce(lambda x, y: x + [Image(self._original)], range(48), [])
+        self._outfp = outfp if outfp else '.'.join([os.path.splitext(fp)[0], 'gif'])
         self._gen_frames = gen
 
     def explodinate(self):
-        filenames = []
-        with imageio.get_writer(self._outfp, mode='I') as writer:
-            for i, f in enumerate(self._gen_frames(self._frames)):
-                _fp = "{2}/{1}__{0}".format(os.path.basename(self._outfp), i, os.path.dirname(self._outfp))
-                filenames.append(_fp)
-                f.save(_fp)
-                writer.append_data(imageio.imread(_fp))
+        out_frames = ImageList()
+        for f in self._gen_frames(self._frames):
+            out_frames.append(f)
+        out_frames.writeImages(self._outfp)
         return self._outfp
 
 
@@ -37,12 +32,26 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         raise Exception("not enough arguments")
 
-    def _randomizer_gen(frames=[]):
+    composite_path = os.path.join(os.path.dirname(__file__), 'resources', 'explodinate_composite.gif')
 
-        for f in frames:
-            mode = 'RGB'
-            overlay = Image.frombytes(mode, f.size, os.urandom(f.size[0]*f.size[1]*len(mode)))
-            mask = Image.frombytes('1', f.size, os.urandom(f.size[0]*f.size[1]*len(mode)))
-            yield Image.composite(f, overlay, mask)
+    def _splode_filter(frame, threshold):
+        for i in range(threshold):
+            frame.transparent(Color('#{06x}'.format(i)))
+        return frame
 
-    Explodinator(sys.argv[1], _randomizer_gen).explodinate()
+    def _filter_gen(frames=[]):
+        for frame in frames:
+            yield _splode_filter(frame, 10)
+
+    def _splode_gen(frames=[]):
+
+        overlay_frames = Image(composite_path)
+        for i, overlay in enumerate(ImageList(overlay_frames)):
+            if i >= len(frames):
+                break
+            size = overlay.geometry
+            f = frames[i].scaled_image(size.width(), size.height())
+            f.composite(_splode_filter(overlay, 10), size)
+            yield f
+
+    Explodinator(sys.argv[1], _splode_gen).explodinate()
